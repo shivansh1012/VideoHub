33b46afa061d70d4b2e7148ce5c0e2c76c69d814
Shivansh Pandey <shivansh.spandey@gmail.com>
2022-07-21 10:21:56 +0530

    Version Change

--- webapp/src/Views/AboutApp/AboutApp.jsx
+++ webapp/src/Views/AboutApp/AboutApp.jsx
@@ -5,10 +5,10 @@ export default function AboutApp() {
             <hr/>
             <h5 style={{margin:"30px"}}>watch videos, get recommendations, search videos and share videos (upload) in the community.</h5>
             <hr/>
-            <h3 style={{margin:"30px"}}>ReactJS : Web App : 4.0.0</h3>
-            <h3 style={{margin:"30px"}}>ExpressJS : Server  : 3.4.0</h3>
-            <h3 style={{margin:"30px"}}>Python : Automation : 1.11.0</h3>
-            <h3 style={{margin:"30px"}}>MongoDB : Database : 0.8.0</h3>
+            <h3 style={{margin:"30px"}}>ReactJS : Web App : 5.2.0</h3>
+            <h3 style={{margin:"30px"}}>ExpressJS : Server  : 4.2.0</h3>
+            <h3 style={{margin:"30px"}}>MongoDB : Database : Schema : 4.0</h3>
+            <h3 style={{margin:"30px"}}>Python : Reader : 2.0.0</h3>
             <hr/>
             <h2 style={{margin:"30px"}}><a href="https://github.com/shivansh1012/VideoHub/" target="_blank" rel="noreferrer">GitHub</a></h2>
         </div>
